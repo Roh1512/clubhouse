96d4c8e16bd1677e8627757472b36f5f109ca7be
Rohith19 <rohith1512a@gmail.com>
2024-09-02 02:55:54 +0530

    bug fixes

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ main();
 
 // Apply rate limiter to all requests
 app.use(limiter);
-/* app.use(
+app.use(
   helmet.contentSecurityPolicy({
     directives: {
       "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
@@ -69,7 +69,7 @@ app.use(limiter);
       ],
     },
   })
-); */
+);
 
 // view engine setup
 /* app.set("views", path.join(__dirname, "views"));

c0193743c597bbd1e99838c67cc696b6658e10f9
Rohith19 <rohith1512a@gmail.com>
2024-09-03 01:07:18 +0530

    One directory structure

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -3,3 +3,5 @@ declare module "react-lazy-load-image-component";
 declare module "*.svg";
 declare module "*.module.css";
 declare module "react-dom/client";
+declare module "@vitejs/plugin-react";
+declare module "vite";

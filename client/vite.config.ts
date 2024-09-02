8bc702c5c5e8bfc069b98b1c415e288a1c1acef6
Rohith19 <rohith1512a@gmail.com>
2024-09-03 02:12:16 +0530

    bugs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig, UserConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(
-  (configEnv: { mode: string }): UserConfig => ({
-    plugins: [react()],
-    server:
-      configEnv.mode === "development"
-        ? {
-            proxy: {
-              "/api": {
-                target: "http://localhost:3000",
-                secure: false,
-              },
-            },
-          }
-        : {},
-  })
-);
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+});

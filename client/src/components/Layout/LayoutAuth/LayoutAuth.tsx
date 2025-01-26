4053aad835ee99a56e4ecceef2c2840daaf0aeb5
Rohith19 <rohith1512a@gmail.com>
2025-01-26 15:34:45 +0530

    navigate from login

--- client/src/components/Layout/LayoutAuth/LayoutAuth.tsx
+++ client/src/components/Layout/LayoutAuth/LayoutAuth.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import logoImage from "../../../assets/logoicon.svg";
 import { Link } from "react-router-dom";
 import ThemeToggle from "../../Buttons/ThemeToggle";
 import Footer from "../../../Footer/Footer";
+import { useAppSelector } from "../../../features/hooks";
+import { RootState } from "../../../features/store";
 
 const LayoutAuth = () => {
+  const { isAuthenticated, user } = useAppSelector(
+    (state: RootState) => state.auth
+  );
+  if (isAuthenticated && user) return <Navigate to="/" />;
   return (
     <>
       <header>
--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -3,12 +3,12 @@ import { useAppSelector } from "../features/hooks";
 import { RootState } from "../features/store";
 
 const ProtectedRoute = () => {
-  const {isAuthenticated, user} = useAppSelector(
+  const { isAuthenticated, user } = useAppSelector(
     (state: RootState) => state.auth
   );
   // console.log(isAuthenticated);
 
-  return (isAuthenticated && user) ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated && user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
